09bfdfd487cb416dd425818a6b5a68a398338dcc
Kaisa S P <kaisakai15@gmail.com>
2021-08-04 15:41:17 +0700

    Updates

---
+++ precache-manifest.4e62bbaf99d101db6f854deff5ad33cb.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4ea9f62de01ed69ed98a70dac803e76",
+    "url": "/react-raka/index.html"
+  },
+  {
+    "revision": "fa5a3ee5b0a2f76bf9a6",
+    "url": "/react-raka/static/css/main.3aeb8986.chunk.css"
+  },
+  {
+    "revision": "c9dda356b7caca6307d0",
+    "url": "/react-raka/static/js/2.d4f6cceb.chunk.js"
+  },
+  {
+    "revision": "fa5a3ee5b0a2f76bf9a6",
+    "url": "/react-raka/static/js/main.2a46b106.chunk.js"
+  },
+  {
+    "revision": "4725b251485c2b924880",
+    "url": "/react-raka/static/js/runtime~main.45bc6839.js"
+  },
+  {
+    "revision": "fb98ed1700e8dfaf0764c11fc36a0a05",
+    "url": "/react-raka/static/media/NunitoSans-Regular.fb98ed17.ttf"
+  },
+  {
+    "revision": "f46f486edefeba66ac4b538dd593d656",
+    "url": "/react-raka/static/media/Quicksand-Regular.f46f486e.ttf"
+  },
+  {
+    "revision": "b5d4adffb6b244532a12c78f4124af70",
+    "url": "/react-raka/static/media/Ubuntu-Bold.b5d4adff.ttf"
+  },
+  {
+    "revision": "f5a896693ead0b135e7e3f7f695463b1",
+    "url": "/react-raka/static/media/Ubuntu-Light.f5a89669.ttf"
+  },
+  {
+    "revision": "6bb9beb4ae22b811bfd5369d517e2f7c",
+    "url": "/react-raka/static/media/Ubuntu-Regular.6bb9beb4.ttf"
+  },
+  {
+    "revision": "b4c6a609d1a3b13dd59b8e7ea03a5c21",
+    "url": "/react-raka/static/media/eksad-logo.b4c6a609.jpg"
+  },
+  {
+    "revision": "226ea55c3d9b36d4388f444c49b513a5",
+    "url": "/react-raka/static/media/fa-brands-400.226ea55c.svg"
+  },
+  {
+    "revision": "659c4d58b00226541ef95c3a76e169c5",
+    "url": "/react-raka/static/media/fa-brands-400.659c4d58.woff2"
+  },
+  {
+    "revision": "8b7a9afd7b95f62e6ee8a72930bfb9ed",
+    "url": "/react-raka/static/media/fa-brands-400.8b7a9afd.woff"
+  },
+  {
+    "revision": "b69de69a4ff8ca0abe96ec0b0c180c5b",
+    "url": "/react-raka/static/media/fa-brands-400.b69de69a.ttf"
+  },
+  {
+    "revision": "ec0716ae8aa1ba781a1a6bcbce833f6c",
+    "url": "/react-raka/static/media/fa-brands-400.ec0716ae.eot"
+  },
+  {
+    "revision": "0b5e3a5451fc62d9023ccafc85bc89db",
+    "url": "/react-raka/static/media/fa-regular-400.0b5e3a54.woff"
+  },
+  {
+    "revision": "6493321d567eb0f22bd5112fbcf044a8",
+    "url": "/react-raka/static/media/fa-regular-400.6493321d.eot"
+  },
+  {
+    "revision": "6993975cbae77892abc356874c645730",
+    "url": "/react-raka/static/media/fa-regular-400.6993975c.svg"
+  },
+  {
+    "revision": "b48c48ea8457846a5695b139c377d3d1",
+    "url": "/react-raka/static/media/fa-regular-400.b48c48ea.ttf"
+  },
+  {
+    "revision": "bdadb6ce95c5a2e7b673940721450d3c",
+    "url": "/react-raka/static/media/fa-regular-400.bdadb6ce.woff2"
+  },
+  {
+    "revision": "2a803d1830aa624f83dc0a79cfadb782",
+    "url": "/react-raka/static/media/fa-solid-900.2a803d18.svg"
+  },
+  {
+    "revision": "48f54f63d7711d0912a9a10205538fc4",
+    "url": "/react-raka/static/media/fa-solid-900.48f54f63.ttf"
+  },
+  {
+    "revision": "bcb927a742a8370b76642fd1a9a749c0",
+    "url": "/react-raka/static/media/fa-solid-900.bcb927a7.woff"
+  },
+  {
+    "revision": "f29ad0031ad2c1c14b771ce504e2bfa7",
+    "url": "/react-raka/static/media/fa-solid-900.f29ad003.eot"
+  },
+  {
+    "revision": "fb493903265cad425ccdf8e04fc2de61",
+    "url": "/react-raka/static/media/fa-solid-900.fb493903.woff2"
+  }
+]);
